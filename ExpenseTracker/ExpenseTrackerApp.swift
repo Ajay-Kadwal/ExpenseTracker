24187d9670d8462962c9892aa4d0c49743b6e21d
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-12-10 19:25:57 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by AJAY KADWAL on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
